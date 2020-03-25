9729e26b1e6b63f198b04512d2cbc2412d9aaeb6
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-03-25 14:58:18 +0530

    (refactor) Tasks completed

---
+++ apps/minikube-registry-helper/configure.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -eu
+
+set -o pipefail 
+
+minikube -p tektontutorial addons enable registry
+
+sleep 5; 
+while (echo && oc get pods -n kube-system --selector='kubernetes.io/minikube-addons=registry' \
+   | grep -v -E "(Running|Completed|STATUS)"); 
+do 
+  sleep 5; 
+done
+
+kubectl apply -n kube-system \
+  -f registry-aliases-sa.yaml \
+  -f registry-aliases-sa-crb.yaml \
+  -f registry-aliases-config.yaml \
+  -f node-etc-hosts-update.yaml \
+  -f patch-coredns-job.yaml
+
+sleep 5; 
+while (echo && kubectl get pods -n kube-system --selector='job-name=registry-aliases-patch-core-dns' \
+   | grep -v -E "(Completed|STATUS)"); 
+do 
+  sleep 5; 
+done
+
+echo "Patch successfully done"
+
+minikube -p tektontutorial  ssh -- sudo cat /etc/hosts
+
+kubectl -n kube-system get cm coredns -o yaml | yq r - data.Corefile
--- bin/start-minikube.sh
+++ bin/start-minikube.sh
@@ -7,14 +7,14 @@ MEMORY=${MEMORY:-8192}
 CPUS=${CPUS:-4}
 
 # Enable if you are going to use Tekton with Knative
-# EXTRA_CONFIG="apiserver.enable-admission-plugins=\
-# LimitRanger,\
-# NamespaceExists,\
-# NamespaceLifecycle,\
-# ResourceQuota,\
-# ServiceAccount,\
-# DefaultStorageClass,\
-# MutatingAdmissionWebhook"
+EXTRA_CONFIG="apiserver.enable-admission-plugins=\
+LimitRanger,\
+NamespaceExists,\
+NamespaceLifecycle,\
+ResourceQuota,\
+ServiceAccount,\
+DefaultStorageClass,\
+MutatingAdmissionWebhook"
 
 minikube start -p $PROFILE_NAME \
   --memory=$MEMORY --cpus=$CPUS \
