450ab2f9c004503f1f04b501ab6b1ae79ef6ec0c
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2020-11-10 23:29:39 +0000

    Add platform specific options (MacOS)
    hyperkit driver should only be used on Mac
    
    don't add --apiserver-names for docker.for.mac.localhost unless minikube is running on a mac

--- bin/start-minikube.sh
+++ bin/start-minikube.sh
@@ -5,6 +5,7 @@ set -eu
 PROFILE_NAME=${PROFILE_NAME:-tektontutorial}
 MEMORY=${MEMORY:-8192}
 CPUS=${CPUS:-4}
+declare PLATFORM_SPECIFIC_OPTIONS=""
 
 # Enable if you are going to use Tekton with Knative
 EXTRA_CONFIG="apiserver.enable-admission-plugins=\
@@ -16,11 +17,17 @@ ServiceAccount,\
 DefaultStorageClass,\
 MutatingAdmissionWebhook"
 
+unamestr=$(uname)
+
+if [ "${unamestr}" == "Darwin" ]; then
+  # Add MacOS specific options for minikube
+  PLATFORM_SPECIFIC_OPTIONS="--driver hyperkit --apiserver-names=docker.for.mac.localhost"
+fi
+
+
 minikube start -p $PROFILE_NAME \
   --memory=$MEMORY --cpus=$CPUS \
   --disk-size=50g \
-  --insecure-registry='10.0.0.0/24' \
-  --driver hyperkit \
-  --apiserver-names=docker.for.mac.localhost
+  --insecure-registry='10.0.0.0/24' ${PLATFORM_SPECIFIC_OPTIONS}
   
 minikube profile $PROFILE_NAME
