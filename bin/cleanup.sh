8185802e8af0f8f755b5144015153575bdb219ba
Kamesh Sampath <kamesh.sampath@hotmail.com>
2021-01-21 15:14:14 +0530

    Release 0.15 (Staging)
    prepare for staging

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+NS=${1:-tektontutorial}
+
 kubectl delete pods --field-selector=status.phase=Succeeded
 kubectl delete pods --field-selector=status.phase=Failed
 
-kubectl delete -n tektontutorial tasks --all
-kubectl delete -n tektontutorial tr --all
-kubectl delete -n tektontutorial pipelines --all
-kubectl delete -n tektontutorial pr --all
-kubectl delete -n tektontutorial pipelineresources
+kubectl delete -n "$NS" pipeline --all
+kubectl delete -n "$NS" pr --all
+kubectl delete -n "$NS" tr --all
+kubectl delete -n "$NS" deploy,svc demo-greeter 2>/dev/null
+kubectl delete -n "$NS" httpproxy demo-greeter 2>/dev/null
+kubectl delete -n "$NS" deploy,svc greeter 2>/dev/null
+kubectl delete -n "$NS" httpproxy greeter 2>/dev/null
+kubectl delete -n "$NS" ksvc greeter 2>/dev/null
